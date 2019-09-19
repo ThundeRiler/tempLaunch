7f9d4dd722e1bc0cb9fda5b3f4588a4da2d78ef9
Cheng Yin <dixindiyirenyc08@gmail.com>
2019-09-19 14:30:47 +1000

    update the organization on pipedrive

--- server/routes/api/counters.js
+++ server/routes/api/counters.js
@@ -20,9 +20,55 @@ module.exports = (app) => {
     let loanAmount = req.body.loanAmount;
     let loanTerm = req.body.loanTerm;
     let loanPurpose = req.body.loanPurpose;
+
+    console.log(postcode)
+
+    //Set up organization, person and deal
+    // Add a new organization
+    const pipedriveOrganizations = new Promise((resolve, reject) => {
+      pipedrive.Organizations.add({'name': companyName}, function(err,addDataOrganizations){
+        var organizationID = addDataOrganizations.id;
+        if (err) throw err;
+        resolve(organizationID);
+        console.log('Create a new Organization')
+      });
+    });
+
+    pipedriveOrganizations.then((organizationID) => {
+      // Add a new person
+      const pipedrivePersons = new Promise((resolve, reject) => {
+        pipedrive.Persons.add({'name':personName, 'email':emailAddress, 'phone': mobileNumber, 'org_id': organizationID},function(err,addDataPersons){
+          var person_orgDict = {'organizationID': organizationID, 'personID': addDataPersons.id};
+          if (err) throw err;
+          resolve(person_orgDict);
+          console.log('Create a new person');
+        });
+      });
+
+      pipedrivePersons.then((person_orgDict) => {const pipedriveDeals = new Promise ((resolve, reject) => {
+        // Add a new Deal
+        pipedrive.Deals.add({'title':companyName, 'value':loanAmount, 'person_id': person_orgDict.personID, 'org_id': person_orgDict.organizationID},function(err,addDataPersons){
+          var deal_person_orgDict = {'organizationID': person_orgDict.organizationID, 'personID': person_orgDict.personID, 'dealID': addDataPersons.id};
+          if (err) throw err;
+          resolve(deal_person_orgDict)
+          console.log('Product 1 was added to deal 1');
+        });
+
+        pipedrive.files.add({'file_path': postcode, 'deal_id': deal_person_orgDict.dealID},function (err,addDataFiles){
+          if (err) throw err;
+          console.log('Add a file for a deal');
+        });
+
+        });
+      });
+    });
+
+
+
+    
     
     // Pipedrive module
-    const pipedriveModule = new Promise((resolve,reject) =>{
+    /*const pipedriveModule = new Promise((resolve,reject) =>{
       // Add a new deal 
       pipedrive.Deals.add({'title':companyName, 'value':loanAmount},function(err,addDataDeals){
         if (err) throw err;
@@ -35,8 +81,8 @@ module.exports = (app) => {
           console.log('Create a new person');
         });
       });
-      
     })
+
     pipedriveModule.then((idDealsPersons) => {
       pipedrive.Activities.add({'subject': 'Send an email to manager.', 
                                 'done': 0, 
@@ -46,7 +92,7 @@ module.exports = (app) => {
         if (err) throw err;
         console.log('Create a new Activities', addDataActivities);
       });
-    })
+    })*/
     
   });
 
